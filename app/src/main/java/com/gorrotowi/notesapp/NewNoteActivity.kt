3516823830a50f5961c85ffdddb8e9dce365bb91
sebastian tellez <sebastian.tellez.o@gmail.com>
2024-06-18 20:56:09 -0600

    Add basic app for notes

---
+++ app/src/main/java/com/gorrotowi/notesapp/NewNoteActivity.kt
@@ -0,0 +1,16 @@
+package com.gorrotowi.notesapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class NewNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_new_note)
+
+    }
+}
